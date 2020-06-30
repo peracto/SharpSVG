b0c009cb2aeca4d30b7680e498a1b4a3a8ba0ca8
Gary Ranson <garyranson@hotmail.com>
2020-06-30 15:25:23 +0100

    aa

---
+++ SharpSvg/Converters/TransformOriginAttributeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Peracto.Svg.Transform;
+using Peracto.Svg.Types;
+
+namespace Peracto.Svg.Converters
+{
+    public class TransformOriginAttributeConverter : AttributeConverterBase<ITransformOrigin>
+    {
+        public TransformOriginAttributeConverter(string name) : base(name)
+        {
+        }
+
+        protected override bool TryCreate(string attributeValue, out ITransformOrigin rc)
+        {
+            var args = Regex.Replace(attributeValue.Trim(), @"[\s,]+", ",")
+                .Split(',');
+            if (args.Length != 2)
+            {
+                rc = null;
+                return false;
+            }
+
+            if (!Measure.TryParse(args[0], MeasureUsage.Horizontal, out var xMeasure) ||
+                !Measure.TryParse(args[1], MeasureUsage.Vertical, out var yMeasure))
+            {
+                rc = null;
+                return false;
+            }
+
+            rc =new TransformOrigin(xMeasure, yMeasure );
+            return true;
+        }
+    }
+}
